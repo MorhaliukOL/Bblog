107b0d8826e606ec207ad24acc0910d83f06cd02
Senku1 <morhaliuk@gmail.com>
2020-03-23 02:09:42 +0200

    CRUD operations for Posts implemented

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import my_blog
+from .views import MyBlog, PostCreate, PostDetail, PostUpdate, PostDelete
 
 urlpatterns = [
-    path('', my_blog, name='my_blog')
+    path('my/', MyBlog.as_view(), name='my-blog'),
+    path('post/create', PostCreate.as_view(), name='post-create'),
+    path('post/detail/<int:pk>', PostDetail.as_view(), name='post-detail'),
+    path('post/update/<int:pk>', PostUpdate.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDelete.as_view(), name='post-delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,60 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, ListView
+from django.urls import reverse_lazy
+from .models import Post
+
+POSTS_PER_PAGE = 10
 
 
-def my_blog(request):
-    return render(request, 'blog/my_blog.html')
+class MyBlog(ListView):
+    """
+    Display list of posts created by the current user,
+    ordered by creation date in reversed order.
+    """
+    context_object_name = 'my_posts'
+    template_name = 'blog/my_blog.html'
+    paginate_by = POSTS_PER_PAGE
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user).order_by('-date')
+
+
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostDetail(DetailView):
+    model = Post
+    context_object_name = 'post'
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'body']
+
+    def get_queryset(self):
+        post = Post.objects.filter(pk=self.kwargs['pk'])
+        if post and post[0].author == self.request.user:
+            return post
+        else:
+            raise Http404()
+
+
+class PostDelete(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('my-blog')
+    template_name = 'blog/post_delete.html'
+
+    def get_queryset(self):
+        post = Post.objects.filter(pk=self.kwargs['pk'])
+        if post and post[0].author == self.request.user:
+            return post
+        else:
+            raise Http404()
